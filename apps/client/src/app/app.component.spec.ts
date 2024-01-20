5e4dfc2d6f872f796ddd184d3ac44f88fff2a09b
Bálint Dósa <dosa.balint@gmail.com>
2024-01-21 00:17:33 +0100

    feat: firebase hosting init

--- apps/client/src/app/app.component.spec.ts
+++ apps/client/src/app/app.component.spec.ts
@@ -14,12 +14,14 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Welcome client'
+    );
   });
 
-  it(`should have as title 'ui'`, () => {
+  it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ui');
+    expect(app.title).toEqual('client');
   });
 });
